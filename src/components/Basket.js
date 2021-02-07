fc8456ef94ad61ab582d697c84c3f2991b8921d1
Kenny <kenny.mckeown@gmail.com>
2021-02-07 16:14:28 +0000

    Ian commit

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,10 +1,19 @@
 import React from "react";
+import styled from 'styled-components';
 
 function Basket() {
 
 
+    const BasketTitle = styled.h3`
+        font-family: Didot, sans-serif;
+        font-size: 50px;
+        color: #e3e3e3;
+        height: 400px;
+        width: 600px;
+    `;
+
     return(
-        <h3>Basket Component</h3>
+        <BasketTitle>The basket view will display all bookings/appointments that the user has made and will allow the user to confirm them.</BasketTitle>
     )
 }
 
