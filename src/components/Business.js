6ef4315ccb1d23f59687132d9fb55976c1becc8b
Mateusz <mateusz.harlos@gmail.com>
2021-02-08 19:06:51 +0000

    login for business profile as sessionStorage

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,11 +1,25 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import BusinessProfile from './BusinessProfile'
 import BusinessLogin from './BusinessLogin';
+import useToken from './useToken';
+
+
+
+// function setToken(userToken) {
+//     sessionStorage.setItem('token', JSON.stringify(userToken));
+// }
+
+// function getToken() {
+//     const tokenString = sessionStorage.getItem('token');
+//     const userToken = JSON.parse(tokenString);
+//     return userToken?.token
+// }
 
 const Business = () => {
     
-    const [token, setToken] = useState();
+    // const token = getToken();
+    const { token, setToken } = useToken();
 
     if(!token) {
       return <BusinessLogin setToken={setToken} />
